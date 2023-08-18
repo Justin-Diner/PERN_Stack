f5f2e6a9ee08f1f633a32849e4b701f24f6597f9
Justin <90215009+DinerJ@users.noreply.github.com>
2023-08-18 17:53:55 -0400

    finished RESTful API

--- server/index.js
+++ server/index.js
@@ -32,10 +32,37 @@ app.use(express.json()); // req.json
   })
 
   // get a todo (get)
+  app.get("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+
+      res.json(todo.rows[0])
+    } catch (err) {
+      console.error(err.message)
+    }
+  })
 
   // update a todo (put)
+  app.put("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body; 
+      const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+
+      res.json("Todo was updated!")
+    } catch (err){
+      console.error(err.message);
+    }
+  })
 
   // delete a todo (delete)
+  app.delete("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [ id ]);
+
+    res.json(`Todo ${id} was deleted.`)
+  })
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
